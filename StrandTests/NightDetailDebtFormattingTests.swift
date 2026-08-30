f15df9082eb93ab8964b885bdd926ef93f06f460
Bernd Helm <bernd.helm@helmundwalter.de>
2026-08-30 11:12:15 +0200

    test(sleep): respect localized debt captions

--- StrandTests/NightDetailDebtFormattingTests.swift
+++ StrandTests/NightDetailDebtFormattingTests.swift
@@ -4,15 +4,15 @@ import StrandDesign
 
 final class NightDetailDebtFormattingTests: XCTestCase {
     func testTenMinuteDebtBoundary() {
-        XCTAssertEqual(nightDetailDebtCaption(9.9), "On target")
+        XCTAssertEqual(nightDetailDebtCaption(9.9), String(localized: "On target"))
         XCTAssertEqual(nightDetailDebtColor(9.9), StrandPalette.statusPositive)
 
-        XCTAssertEqual(nightDetailDebtCaption(10.0), "Below need")
+        XCTAssertEqual(nightDetailDebtCaption(10.0), String(localized: "Below need"))
         XCTAssertEqual(nightDetailDebtColor(10.0), StrandPalette.statusWarning)
     }
 
     func testImportedDebtAboveBoundaryRemainsDebt() {
-        XCTAssertEqual(nightDetailDebtCaption(12.5), "Below need")
+        XCTAssertEqual(nightDetailDebtCaption(12.5), String(localized: "Below need"))
         XCTAssertEqual(nightDetailDebtColor(12.5), StrandPalette.statusWarning)
     }
 }
